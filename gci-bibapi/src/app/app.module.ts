92c68ca42d9562b4e15eace4616bf3ae605f2435
Israel Oluwole <olaleyeisrael@gmail.com>
2018-12-10 00:13:25 +0100

    changed some configs

--- gci-bibapi/src/app/app.module.ts
+++ gci-bibapi/src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { MiddleComponent } from './middle/middle.component';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    MiddleComponent,
+    SignupComponent,
+    SigninComponent,
+    DashboardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatTabsModule,
+    FormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 mongoose
   .connect(
-    process.env.M_DURI,
+    process.env.M_PURI,
     { useNewUrlParser: true, useCreateIndex: true, autoIndex: true }
   )
   .then(resp => console.log(`[Mongodb Server] started on port 27017`))
