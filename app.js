7d4dfe58da12b5d84542530d9636555ca5942f61
Israel Oluwole <olaleyeisrael@gmail.com>
2019-04-25 16:44:36 +0100

    fixed cors errors

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(bodyParser.json());
 // [middleware] address cors related issues
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
   res.header(
       'Access-Control-Allow-Headers',
