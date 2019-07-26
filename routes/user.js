c62a4dfff61d210e91a20e31624d2983480512cb
Israel Oluwole <olaleyeisrael@gmail.com>
2019-07-26 20:48:07 +0100

    bm: removed user from it route implementation

--- routes/user.js
+++ routes/user.js
@@ -2,31 +2,32 @@
 const Route = require('express').Router();
 
 const {
-  signUp, signIn,
-  verify, forgot, retrieve,
-  modify, listUsers, deleteUsers,
+  // signUp, signIn,
+  // verify, forgot, retrieve,
+  // modify,
+  listUsers, deleteUsers,
   upgrade, paymentsResp,
 } = require('../controllers/user');
 const checkAuth = require('../middlewares/check-auth');
 
-// [Routes]
-// [post] add users to db --> admin
-Route.post('/signup', signUp);
-
-// [get] user signs in to get token
-Route.put('/signin', signIn);
-
-// [patch] first link to password recovery
-Route.patch('/verify/', verify);
-
-// [put] first link to password recovery
-Route.put('/forgot', forgot);
-
-// [patch] retrieve password
-Route.patch('/retrieve', retrieve);
-
-// [patch] edit user details in db --> user && admin
-Route.patch('/modify', checkAuth, modify);
+// // [Routes]
+// // [post] add users to db --> admin
+// Route.post('/signup', signUp);
+//
+// // [get] user signs in to get token
+// Route.put('/signin', signIn);
+//
+// // [patch] first link to password recovery
+// Route.patch('/verify/', verify);
+//
+// // [put] first link to password recovery
+// Route.put('/forgot', forgot);
+//
+// // [patch] retrieve password
+// Route.patch('/retrieve', retrieve);
+//
+// // [patch] edit user details in db --> user && admin
+// Route.patch('/modify', checkAuth, modify);
 
 // [get] user lists/details from db --> admin
 Route.get('/lists', checkAuth, listUsers);
