117a7d909d83653d7cbce47eb267dc57df38bcff
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-02 13:22:45 +0100

    bm: beta imp for keys model

---
+++ models/keys.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+module.exports = mongoose.model('key', new mongoose.Schema({
+  key: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: Boolean,
+    required: true,
+    enum: [true, false],
+    default: true,
+  },
+  user: {
+    type: String,
+    required: true,
+  },
+}));
